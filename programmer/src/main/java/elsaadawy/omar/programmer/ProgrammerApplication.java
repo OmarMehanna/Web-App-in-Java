a27c4df36cc9cda27203aafc51ab764385af03f9
OmarElsaadawy <omehanny@gmail.com>
2024-08-12 18:59:01 -0300

    initial commit

---
+++ programmer/src/main/java/elsaadawy/omar/programmer/ProgrammerApplication.java
@@ -0,0 +1,13 @@
+package elsaadawy.omar.programmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammerApplication.class, args);
+	}
+
+}
---
+++ programmer/src/test/java/elsaadawy/omar/programmer/ProgrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package elsaadawy.omar.programmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
