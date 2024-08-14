a67b67b8719a31bc795eb745f9e5c02959bbdd24
OmarElsaadawy <omehanny@gmail.com>
2024-08-14 18:33:33 -0300

    More understanding of Spring

--- programmer/src/main/java/elsaadawy/omar/programmer/ProgrammerApplication.java
+++ programmer/src/main/java/elsaadawy/omar/programmer/ProgrammerApplication.java
@@ -1,13 +1,16 @@
 package elsaadawy.omar.programmer;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; //importing the SpringApplication class
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProgrammerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProgrammerApplication.class, args);
+
+		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
+		System.out.println(welcomeMessage);
+
 	}
 
 }
